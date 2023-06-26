5b6e5446ae0d676d7befcb7af892665e647fc3d2
nizwan567 <137814681+nizwan567@users.noreply.github.com>
2023-06-27 00:30:16 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+
+model = pickle.load(open(r'C:\Users\Desktop\Desktop\ds_project\kaggle\mango/model.pkl', 'rb'))
+
+def main():
+    # Set the app title
+    st.title("Mango Grade Prediction")
+
+    # Create input fields for weight, length, and circumference
+    weight = st.number_input("Weight (g)")
+    length = st.number_input("Length (cm)")
+    circumference = st.number_input("Circumference (cm)")
+
+    # Create a button for prediction
+    if st.button("Predict"):
+        # Create a DataFrame with the user's input
+        data = pd.DataFrame({'Weight': [weight], 'Length': [length], 'Circumference': [circumference]})
+
+        # Use the loaded model for prediction
+        prediction = model.predict(data)
+
+        # Display the predicted grade
+        st.success(f"Your mango grade is {prediction[0]}")
+
+main()
